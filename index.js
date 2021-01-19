74c8b6627ef2a9cf2a6d815b46854e477b28b57b
Armymn6549 <72281712+Armymn6549@users.noreply.github.com>
2021-01-19 15:17:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const Discord = require('discord.js');
 Discord.Constants.DefaultOptions.ws.properties.$browser = "Discord Android"
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
+client.on("ready", () =>{
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({
+        status: "online",  // You can show online, idle... Do not disturb is dnd
+        game: {
+            name: "!help",  // The message shown
+            type: "PLAYING" // PLAYING, WATCHING, LISTENING, STREAMING,
+        }
+    });
+ });
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong!');
